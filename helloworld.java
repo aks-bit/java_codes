a35c731a5213635968d5dccb46195206a6493fac
aks-bit <aksembfw@gmail.com>
2023-03-18 22:37:40 +0530

    commit-1

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, welcome to java coding.\n");
+        System.out.println("This is my first program.");
+    }
+}
